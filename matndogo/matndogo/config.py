1110c3349bae713e154c031318b8892123121fa1
ojay <jonathanonderi2018@yahoo.com>
2021-07-14 22:22:28 +0300

    update

--- matndogo/matndogo/config.py
+++ matndogo/matndogo/config.py
@@ -1,6 +1,7 @@
 secret_key = 'django-insecure-$ool72)nq4h*kb&&wt#+fq0h+jki%mr3r4-!=n)0v=qew=yuj)'
 
 class EmailConf:
+    
     EMAIL_HOST_USER = "flexearn20@gmail.com"
     EMAIL_HOST_PASSWORD = "kemvoxmachxlssht"
     EMAIL_HOST = "smtp.gmail.com"
