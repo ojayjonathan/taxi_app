9d36a97bd337f9540d6099e883db727b15ed6132
ojay <jonathanonderi2018@yahoo.com>
2021-07-26 17:25:21 +0300

    update

--- matndogo/matndogo/settings.py
+++ matndogo/matndogo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['192.168.43.60', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -53,6 +54,7 @@ REST_FRAMEWORK = {
 }
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
